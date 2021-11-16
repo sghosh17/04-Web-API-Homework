a6449c7a6d023618b3608cc4a7e2cf5984ff6fd2
Sampreeti Das <sampreetighosh@gmail.com>
2021-11-16 14:30:48 +0000

    Upadted thr timer function

--- Assets/script.js
+++ Assets/script.js
@@ -8,4 +8,33 @@ startEl.addEventListener("click", function (event) {
   var nextinfoEl = infoEl.nextElementSibling;
   nextinfoEl.classList.remove("hidden");
   nextinfoEl.classList.add("visible");
+
+  var max_time = 15;
+  display = document.querySelector("#time");
+  startTimer(max_time, display);
 });
+
+// Timer
+function startTimer(duration, display) {
+  var timer = duration;
+  var minutes, displayedMinutes;
+  var seconds, displayedSeconds;
+  var set_interval = setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    displayedMinutes = minutes < 10 ? "0" + minutes : minutes;
+    displayedSeconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = displayedMinutes + ":" + displayedSeconds;
+
+    if (--timer < 0) {
+      timer = duration;
+    }
+
+    if (seconds === 0) {
+      alert("Game Over!");
+      clearInterval(set_interval);
+    }
+  }, 1000);
+}
